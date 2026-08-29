182bd2c929fcff4f75bbea53de5086782274621e
thayanithi15-git <thayanithi2006s@gmail.com>
2026-08-29 15:57:07 +0530

    Initial Setup

--- powershell.cmd
+++ powershell.cmd
@@ -1,2 +1,3 @@
 @echo off
+set PATH=C:\Windows\System32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0;C:\Program Files\nodejs;%PATH%
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe %*
